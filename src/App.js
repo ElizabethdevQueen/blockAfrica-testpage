63a9a263692c77a3d2e7438c89d0991b12b1ff4a
Elizabeth O. Segun-Ajiboye <66387727+ElizabethdevQueen@users.noreply.github.com>
2021-04-17 23:35:36 +0100

    main component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CssBaseline from "@material-ui/core/CssBaseline";
+// import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import { Box, makeStyles } from "@material-ui/core";
+import Header from "./components/Headers";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: "#cfe8fc",
+    height: "auto",
+    padding: "2rem"
+  },
+  mainBox: {
+    backgroundColor: "#fff",
+    height: "100vh",
+    width: "50%",
+    borderRadius: "3rem",
+    paddingTop: "2rem",
+    border: "12px solid #000",
+    margin: "auto"
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Container fixed className={classes.root}>
+        <Box className={classes.mainBox}>
+          <Header />
+          <CssBaseline />
+        </Box>
+      </Container>
+    </>
   );
 }
-
-export default App;
