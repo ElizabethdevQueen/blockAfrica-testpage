62e3378f14aa72fc09f6b8884cd7f3348cc63875
Elizabeth O. Segun-Ajiboye <66387727+ElizabethdevQueen@users.noreply.github.com>
2021-04-19 00:37:57 +0100

    The main App routed in this component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import CssBaseline from "@material-ui/core/CssBaseline";
-// import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
-import { Box, makeStyles } from "@material-ui/core";
-import Header from "./components/Headers";
-import InfoBar from "./components/InfoBar";
-import SelectCurrency from "./components/SelectCurrency";
-import RecentActivity from "./components/RecentActivity";
-import FooterNavbar from "./components/FooterNavbar";
+import Home from "./pages/Home"
+import Activity from "./pages/Activity";
+import Wallet from "./pages/Wallet";
+import Market from "./pages/Market";
+import Earn from "./pages/Earn";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: "#cfe8fc",
-    height: "auto",
-    padding: "2rem"
-  },
-  mainBox: {
-    backgroundColor: "#fff",
-    height: "auto",
-    width: "40%",
-    borderRadius: "3rem",
-    paddingTop: "2rem",
-    border: "12px solid #000",
-    margin: "auto"
-  },
-}));
 
 export default function App() {
-  const classes = useStyles();
-
   return (
-    <>
+    <Router>
       <CssBaseline />
-      <Container fixed className={classes.root}>
-        <Box className={classes.mainBox}>
-          <Header />
-          <InfoBar />
-          <SelectCurrency />
-          <RecentActivity />
-          <FooterNavbar />
-        </Box>
-      </Container>
-    </>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/activity" component={Activity} />
+        <Route path="/wallet" component={Wallet} />
+        <Route path="/market" component={Market} />
+        <Route path="/earn" component={Earn} />
+      </Switch>
+    </Router>
   );
 }
