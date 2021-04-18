b47a923aae3aa63820d92fe364491ce5cc390a7b
Elizabeth O. Segun-Ajiboye <66387727+ElizabethdevQueen@users.noreply.github.com>
2021-04-19 00:39:30 +0100

    currency input section

--- src/components/SelectCurrency.jsx
+++ src/components/SelectCurrency.jsx
@@ -10,27 +10,37 @@ import {
 
 const SelectCurrency = () => {
   return (
-      <div style={{ paddingTop: "2rem", backgroundColor:"grey" }}>
-    <Box bg="white" w="100%" p={4}>
-      <p style={{ paddingBottom: "1rem", fontSize:"1rem", fontWeight: 500, }}>Select Currency:</p>
-      <InputGroup>
-        <InputLeftAddon children="AFYA" />
-        <Input type="tel" placeholder="0.00000001" />
-      </InputGroup>
-      <Stack
-        direction="row"
-        align="center"
-        justifyContent="space-around"
-        marginTop="1rem"
+    <div style={{ paddingTop: "2rem", backgroundColor: "#CCC9C9" }}>
+      <p
+        style={{
+          paddingBottom: "1rem",
+          paddingLeft: "1rem",
+          fontSize: "1rem",
+          fontWeight: 500,
+          color: "#4791db",
+        }}
       >
-        <Button bg="#4791db" color="#fff" size="lg" alignItems="center">
-          SEND
-        </Button>
-        <Button bg="#4791db" color="#fff" size="lg">
-          RECEIVE
-        </Button>
-      </Stack>
-    </Box>
+        Select Currency:
+      </p>
+      <Box bg="white" w="100%" p={4}>
+        <InputGroup>
+          <InputLeftAddon children="AFYA" />
+          <Input type="tel" placeholder="0.00000001" />
+        </InputGroup>
+        <Stack
+          direction="row"
+          align="center"
+          justifyContent="space-around"
+          marginTop="1rem"
+        >
+          <Button bg="#4791db" color="#fff" size="lg" alignItems="center" fontWeight="light">
+            SEND
+          </Button>
+          <Button bg="#4791db" color="#fff" size="lg" fontWeight="light">
+            RECEIVE
+          </Button>
+        </Stack>
+      </Box>
     </div>
   );
 };
